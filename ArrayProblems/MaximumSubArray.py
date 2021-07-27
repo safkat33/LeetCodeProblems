e4b7c568a21c42b125a0b4b922c7ac0790d68d61
safkat.arefin <safkat.arefin@co-well.jp>
2021-07-27 23:47:12 +0900

    Added DP solutiion for MaxSubArray

--- ArrayProblems/MaximumSubArray.py
+++ ArrayProblems/MaximumSubArray.py
@@ -2,5 +2,11 @@ from typing import List
 
 
 class Solution:
+
     def maxSubArray(self, nums: List[int]) -> int:
-        pass
+        max_sum = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i] + nums[i - 1]:
+                nums[i] = nums[i] + nums[i - 1]
+            max_sum = max(nums[i], max_sum)
+        return max_sum
