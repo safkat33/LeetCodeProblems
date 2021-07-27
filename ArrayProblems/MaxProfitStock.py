9c99ff0bb2e8d69d52b521d06fc1ce9c57390e31
safkat.arefin <safkat.arefin@co-well.jp>
2021-07-28 01:08:21 +0900

    Solve the maxprofit Problem

--- ArrayProblems/MaxProfitStock.py
+++ ArrayProblems/MaxProfitStock.py
@@ -3,4 +3,11 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        profit = 0
+        buying_price = prices[0]
+        for i in range(1, len(prices)):
+            if prices[i] < buying_price:
+                buying_price = prices[i]
+            elif prices[i] - buying_price > profit:
+                profit = prices[i] - buying_price
+        return profit
