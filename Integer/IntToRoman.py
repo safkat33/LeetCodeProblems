282e0cc3fe3f24d4b27e2833a49a779d93866366
safkat33 <safkat.33@gmail.com>
2021-08-08 14:33:26 +0900

    Changed the IntToRoman Solution

--- Integer/IntToRoman.py
+++ Integer/IntToRoman.py
@@ -26,35 +26,35 @@ Output: "III"
 class Solution:
     def intToRoman(self, num: int) -> str:
         res = ""
-        if num//1000 > 0:
+        if num >= 1000:
             for i in range(num//1000):
                 res += "M"
             num %= 1000
-        if num//900 > 0:
+        if num >= 900:
             res += "CM"
             num %= 900
-        if num//500 > 0:
+        if num >= 500:
             for i in range(num//500):
                 res += "D"
             num %= 500
-        if num//400 > 0:
+        if num >= 400:
             res += "CD"
             num %= 400
-        if num//100 > 0:
+        if num >= 100:
             for i in range(num//100):
                 res += "C"
             num %= 100
-        if num//90 > 0:
+        if num >= 90:
             res += "XC"
             num %= 90
-        if num//50 > 0:
+        if num >= 50:
             for i in range(num//50):
                 res += "L"
             num %= 50
-        if num//40 > 0:
+        if num >= 40:
             res += "XL"
             num %= 40
-        if num//10 > 0:
+        if num >= 10:
             for i in range(num//10):
                 res += "X"
             num %= 10
