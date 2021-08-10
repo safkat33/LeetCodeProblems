5741a7ec0ec3d8dd5a6dd4ad29b19d48c3a69512
safkat33 <safkat.33@gmail.com>
2021-08-10 12:03:33 +0900

    Added solution for Longest Common Prefix

--- String/LongestCommonPrefix.py
+++ String/LongestCommonPrefix.py
@@ -16,3 +16,25 @@ Input: strs = ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
 """
+from typing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        if len(strs) == 1:
+            return strs[0]
+        strs.sort(key = len)
+        for i in range(len(strs[0])):
+            exist = False
+            for j in range(1, len(strs)):
+                if strs[0][i] == strs[j][i]:
+                    exist = True
+                else:
+                    exist = False
+                    break
+            if exist:
+                res += strs[0][i]
+            else:
+                break
+        return res
