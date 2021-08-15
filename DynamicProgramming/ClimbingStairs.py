e468dde2b38b7fbd14c81338d59f31354edc69b6
safkat33 <safkat.33@gmail.com>
2021-08-16 02:43:00 +0900

    Added solution for climbing stairs

--- DynamicProgramming/ClimbingStairs.py
+++ DynamicProgramming/ClimbingStairs.py
@@ -16,3 +16,14 @@ Explanation: There are two ways to climb to the top.
 practice
 steps = |1|2|3|5|8
 """
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        res = [1] * n
+        for i in range(1, n):
+            if i == 1:
+                res[i] += res[i - 1]
+            else:
+                res[i] = res[i - 1] + res[i - 2]
+        return res[-1]
