97a65b55a2706c668ddee394002ff1eec75dcfa2
safkat33 <safkat.33@gmail.com>
2021-08-16 01:17:42 +0900

    Added solution for Unique path

--- DynamicProgramming/UniquePath.py
+++ DynamicProgramming/UniquePath.py
@@ -11,3 +11,12 @@ Example 1:
 Input: m = 3, n = 7
 Output: 28
 """
+
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for i in range(n)] for i in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        return dp[-1][-1]
