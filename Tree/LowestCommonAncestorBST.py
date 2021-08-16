00da5ccfe04045a65c1ec0183cda6f227763a320
safkat33 <safkat.33@gmail.com>
2021-08-17 01:53:18 +0900

    Added solution for Lowest Common Ancestor of a Binary Search Tree

--- Tree/LowestCommonAncestorBST.py
+++ Tree/LowestCommonAncestorBST.py
@@ -12,3 +12,23 @@ Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
 Output: 6
 Explanation: The LCA of nodes 2 and 8 is 6.
 """
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        small_value = min(p.val, q.val)
+        large_value = max(p.val, q.val)
+        while root:
+            if small_value <= root.val <= large_value:
+                break
+            elif root.val < small_value:
+                root = root.right
+            elif root.val > large_value:
+                root = root.left
+        return root
